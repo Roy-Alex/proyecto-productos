59aad1bcdfbc5d866b5d1a5f0732484029564a61
Matias Ramos <roldangoatemanuel@gmail.com>
2025-11-01 16:46:13 -0500

    modificado

--- crud-productos/src/main/java/com/example/crud_productos/repository/ProductRepository.java
+++ crud-productos/src/main/java/com/example/crud_productos/repository/ProductRepository.java
@@ -1,6 +1,6 @@
-package com.example.crud-productos.repository;
+package com.example.crud_productos.repository;
 
-import com.example.crud-productos.model.Product;
+import com.example.crud_productos.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
